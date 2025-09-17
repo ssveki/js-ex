9450c37f27b2f7f3f4fd666794763467b8f0c658
ssveki <sosninav4@gmail.com>
2025-09-17 20:59:23 +0900

    first practisse

---
+++ Пр задание №1/script.js
@@ -0,0 +1,35 @@
+/* Уровень 1
+
+const name = prompt("Введите имя: ");
+const surname = prompt("Введите фамилию: ");
+const age = Number(prompt("Введите возраст: "));
+
+console.log(`Привет, ${name} ${surname}! Тебе ${age} лет.`);
+*/
+
+/* Уровень 2
+
+const num = prompt("Введите число: ");
+
+if (isNaN(num)) {
+    console.log(`${num} не число`)
+} else {
+    if (num % 2 === 0) {
+        console.log(`Число ${num} чётное`)}
+    else {
+        console.log(`Число ${num} нечётное`)
+    }
+}
+*/
+
+const name = prompt("Введите имя: ");
+const age = Number(prompt("Введите возраст: "));
+
+if (age < 18) {
+        console.log(`Привет, ${name}! Ты ещё школьник.`)}
+    else if (age < 65){
+        console.log(`Привет, ${name}! Ты взрослый человек, заходи.`)
+    }
+    else {
+        console.log(`Привет, ${name}! Для возрастных лиц у нас скидки.`)
+    }
