f1906159bee634fbfc486c8b2e2ed9bd94ed312e
ssveki <sosninav4@gmail.com>
2026-01-21 14:42:51 +0900

    fix bug

--- fizzbuzz/fizzbuzz.js
+++ fizzbuzz/fizzbuzz.js
@@ -1,13 +1,17 @@
 for (let i = 1; i <= 100; i++) {
-	if (i % 3 == 0) {
+	if (i % 3 === 0 && i % 5 === 0) {
+        console.log('FizzBuzz')
+    }
+    
+    else if (i % 3 === 0) {
 		console.log('Fizz');
 	}
 
-	if (i % 5 == 0) {
+	else if (i % 5 === 0) {
 		console.log('Buzz');
 	}
 
-	if ((i % 3 != 0) && (i % 5 != 0)) {
-		console.log(i);
-	}
+    else {
+        console.log(i)
+    }
 }
